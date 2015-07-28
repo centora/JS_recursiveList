afe98ca47f3fe37278b6fba1adb6d792432338b6
Centora <oksana.blonskaya@gmail.com>
2015-07-28 22:15:51 +0300

    Recursive list [0.0.3]

---
+++ js/main.js
@@ -0,0 +1,25 @@
+(function(){
+    function recursiveList (data) {
+        var container = document.getElementById('list'),
+        	list = document.createElement("ul"),
+        	listItem;
+
+    	container.appendChild(list);
+
+        for(var i = 0; i < data.length; i++){
+			listItem = document.createElement("li");
+
+        	if( !(data[i].constructor.toString().indexOf('Array') > -1) ){
+            	listItem.innerHTML = data[i];
+        	}else{
+        		listItem.appendChild(recursiveList (data[i]));
+        	}
+            list.appendChild(listItem);
+        }
+
+        return list;
+    }
+
+    recursiveList ( [1,2,[3,4,[5,6,7],8],9,10] );
+
+})();
