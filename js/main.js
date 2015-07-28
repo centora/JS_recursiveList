febc5812ba197ee4cc72668452e0998395d5f751
Centora <oksana.blonskaya@gmail.com>
2015-07-28 22:30:13 +0300

    Recursive list [0.0.8]

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
-(function(){
+(function(){   
     function recursiveList (data) {
-        var container = document.getElementById('list'),
-        	list = document.createElement("ul"),
-        	listItem;
 
-    	container.appendChild(list);
+        var container = document.getElementById('list'),
+            list = document.createElement("ul"),
+            listItem;
+
+        container.appendChild(list);
 
         for(var i = 0; i < data.length; i++){
-			listItem = document.createElement("li");
+            listItem = document.createElement("li");
 
-        	if( !(data[i].constructor.toString().indexOf('Array') > -1) ){
-            	listItem.innerHTML = data[i];
-        	}else{
-        		listItem.appendChild(recursiveList (data[i]));
-        	}
+            if( !(data[i].constructor.toString().indexOf('Array') > -1) ){
+                listItem.innerHTML = data[i];
+            }else{
+                listItem.appendChild(recursiveList (data[i]));
+            }
             list.appendChild(listItem);
         }
 
